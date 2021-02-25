cf9a881d912fe285834e22362f48d136b369e378
ruanzhihao <ruanzhihao99@163.com>
2021-02-25 14:41:07 +0800

    第二次提交

--- src/com/java/HelloGit.java
+++ src/com/java/HelloGit.java
@@ -3,6 +3,7 @@ package com.java;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello Git!");
-         System.out.println("hello Git2!"); 
+        System.out.println("hello Git2!");
+        System.out.println("BH");
     }
 }
