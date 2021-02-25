405d0a6bb296d8284ba5b1621603fd27d70b375c
ruanzhihao <57793919+ruanzhihao@users.noreply.github.com>
2021-02-25 14:36:51 +0800

    平台修改
    平台修改

--- src/com/java/HelloGit.java
+++ src/com/java/HelloGit.java
@@ -3,5 +3,6 @@ package com.java;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+         System.out.println("hello Git2!"); 
     }
 }
